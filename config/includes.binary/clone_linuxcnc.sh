f9704c1292518d84c9b47f3bb07237d21d912552
probotix <len@probotix.com>
2024-04-22 11:42:55 -0500

    ignoring clone_linuxcnc.sh

--- config/includes.binary/clone_linuxcnc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/probotix/
-git config --global user.email "len@probotix.com"
-git config --global user.name "probotix"
-git clone https://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@github.com/probotix/linuxcnc.git
-
-
